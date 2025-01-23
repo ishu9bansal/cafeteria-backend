c1551b12306250c6cf42b2b70d6e04ab13a6ffe4
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-23 22:55:14 +0530

    add validations

--- models/dish.js
+++ models/dish.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 const DishSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    price: { type: Number, required: true },
+    price: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value > 0;
+            },
+            message: 'Price must be greater than zero.'
+        }
+    },
     inStock: { type: Boolean, default: true },
     counter: { type: mongoose.Schema.Types.ObjectId, ref: 'Counter', required: true },
 });
--- models/user.js
+++ models/user.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
 const CartItemSchema = new mongoose.Schema({
-    dish: { type: mongoose.Schema.Types.ObjectId, ref: 'Dish', required: true },
-    quantity: { type: Number, required: true },
+    dish: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dish',
+        required: true,
+        validate: {
+            validator: async function (dishId) {
+                const dishExists = await mongoose.model('Dish').exists({ _id: dishId });
+                return dishExists;
+            },
+            message: 'Invalid dish reference.'
+        }
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: [1, 'Quantity must be at least 1.']
+    },
 });
 
 const UserSchema = new mongoose.Schema({

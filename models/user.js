dbf5ced388233c837af9c00d5720fc72098a75d5
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-28 10:12:55 +0530

    modify user model to include password

--- models/user.js
+++ models/user.js
@@ -24,6 +24,7 @@ const CartItemSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     role: { type: String, enum: Object.keys(ROLE), required: true },
     cart: { type: [CartItemSchema] },
 });

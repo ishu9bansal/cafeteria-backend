33561cc1c05a5c0cdbcfc7c5061cbd49d9919836
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-28 11:59:23 +0530

    use jwt for auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,23 @@
 const User = require("../models/user");
+const jwt = require('jsonwebtoken');
 
 async function auth(req, res, next) {
-    const id = req.headers['x-userid'];
-    try {
-        req.user = await User.findById(id).populate('cart.dish');
-    } catch (err) {
-        console.error(err);
-        return res.status(400).json({ message: "Please login to continue!" });
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: "Please login to continue!" });
     }
-    if (req.user) {
-        next();
-    } else {
+    try {
+        const userInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const { userId } = userInfo;
+        req.user = await User.findById(userId).populate('cart.dish');
+    } catch (err) {
+        return res.status(403).json({ message: err.message });
+    }
+    if (!req.user) {
         return res.status(404).json({ message: "User not found!" });
     }
+    next();
 }
 
 module.exports = {
--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,79 @@
 const express = require('express');
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+let refreshTokens = new Set();
+
 router.post('/login', async (req, res) => {
-    const { email } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(400).json({ message: "User not found" });
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Username not registered!" });
+        }
+
+        const passwordMatched = true || await bcrypt.compare(password, user.password);
+
+        if (passwordMatched) {
+            const userInfo = { userId: user._id };
+            const token = generateAccessToken(userInfo);
+            const refreshToken = jwt.sign(userInfo, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1h' });
+            refreshTokens.add(refreshToken);
+            return res.json({ token: token, refreshToken: refreshToken });
+        } else {
+            return res.status(401).json({ message: "Authentication failed!" });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
     }
-    const userId = user._id;
-    res.json({ token: "", refreshToken: "", userId });
 });
 
 router.post('/register', async (req, res) => {
-    const { name, email } = req.body;
-    const user = new User({ name, email, role: 'Customer' });
-    await user.save();
-    res.status(201).json(user);
+    const { name, email, password } = req.body;
+    try {
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(password, salt);
+        const user = new User({ name, email, role: 'Customer', password: hash });
+        await user.save();
+        delete user.password;
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post("/token", async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader && authHeader.split(' ')[1];
+    if (!refreshToken || !refreshTokens.has(refreshToken)) {
+        return res.status(401).json({ message: "Please login to continue!" });
+    }
+    try {
+        const userInfo = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const { userId } = userInfo;
+        const token = generateAccessToken({ userId });
+        return res.json({ token });
+    } catch (err) {
+        return res.status(403).json({ message: err.message });
+    }
 });
 
 router.delete('/logout', async (req, res) => {
-    res.status(204).json({ message: 'user logged out' });
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token || !refreshTokens.has(token)) {
+        return res.status(200).json({ message: "Already logged out" });
+    }
+    refreshTokens.delete(token);
+    res.status(204).json({ message: 'User logged out' });
 });
+
+function generateAccessToken(userInfo) {
+    return jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+}
 
 module.exports = router;

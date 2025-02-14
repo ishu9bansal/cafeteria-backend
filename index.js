568c133c6f0a3586a0997e0429b53d02f6e0aded
Ishu Bansal <ishubansal1400@gmail.com>
2025-02-14 13:36:24 +0530

    require dotenv in dev only

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Basic Setup for Backend with Express and Mongoose
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 require('./config/connection');
 const express = require('express');
 const cors = require('cors');
